8ee69c3f77d6c4940e9c23d41d5cfa78b3368a1f
Attia Ahmed <attia.a.ahmed@gmail.com>
2022-11-19 20:53:42 +0300

    translate

---
+++ lang/en/notification.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'new_news' => [
+        'title' => 'You got fresh news',
+        'body' => 'Title: :title',
+    ],
+    'new_report' => [
+        'title' => 'New Report',
+        'body'  => 'New Report',
+    ],
+    'new_comment' => [
+        'title' => 'New Comment',
+        'body'  => ':name commented on your post ":comment"',
+    ],
+    'new_post' => [
+        'title' => 'New Like',
+        'body'  => ':name liked your post ":content"',
+    ],
+    'new_comment_report' => [
+        'title' => 'Your comment is under review',
+        'body'  => 'Your comment ":comment" has been reported and is under review',
+    ],
+    'new_post_reported' => [
+        'title' => 'Your post is under review',
+        'body'  => 'Your post ":content" has been reported and is under review',
+    ],
+    'user_blocked' => [
+        'title' => 'Your account is deactivate',
+        'body'  => 'Your account is deactivate from posting and commenting for :period due to repeated reports',
+    ],
+];
--- lang/en/passwords.php
+++ lang/en/passwords.php
@@ -18,5 +18,5 @@ return [
     'throttled' => 'Please wait before retrying.',
     'token' => 'This password reset token is invalid.',
     'user' => "We can't find a user with that email address.",
-
+    'change' => 'Password was changed',
 ];
