2391b9269c1c64621b9203e61aedc8fc1e883bae
Mohamed AlWhaidi <49125190+MohamedAlwhaidi@users.noreply.github.com>
2022-11-25 10:55:59 +0200

    add config services

--- config/services.php
+++ config/services.php
@@ -31,4 +31,15 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'google' => [
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => env('GOOGLE_REDIRECT_URI')
+    ],
+    'apple' => [
+        'client_id' => env('APPLE_CLIENT_ID'),
+        'client_secret' => env('APPLE_CLIENT_SECRET'),
+        'redirect' => env('APPLE_REDIRECT_URI')
+    ],
+
 ];
