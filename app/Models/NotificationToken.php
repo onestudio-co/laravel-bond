7f5b9b759e0fca941415f8267e348420bb59270c
Mohamed AlWhaidi <49125190+MohamedAlwhaidi@users.noreply.github.com>
2022-12-09 14:27:25 +0200

    use fluent in NotificationToken Model

--- app/Models/NotificationToken.php
+++ app/Models/NotificationToken.php
@@ -4,10 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 class NotificationToken extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+    private int $id;
+
+    protected int $user_id;
+
+    protected string $token;
+
+    protected string $device_id;
+
+    protected string $device_type;
+
+    protected Carbon $created_at;
+
+    protected ?Carbon $updated_at;
 }
