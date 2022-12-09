f9aded6a8b47d0a1058cd7fd8890398febadb90d
Mohamed AlWhaidi <49125190+MohamedAlwhaidi@users.noreply.github.com>
2022-12-09 14:10:39 +0200

    use fluent in SocialLogin Model

--- app/Models/SocialLogin.php
+++ app/Models/SocialLogin.php
@@ -2,8 +2,11 @@
 
 namespace App\Models;
 
+use Based\Fluent\Relations\Relation;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Carbon;
 
 class SocialLogin extends Model
 {
@@ -11,7 +14,26 @@ class SocialLogin extends Model
 
     protected $guarded = ['id'];
 
-    public function user()
+    private int $id;
+
+    protected int $user_id;
+
+    protected string $provider;
+
+    protected string $provider_id;
+
+    protected string $email;
+
+    protected string $last_token;
+
+    protected Carbon $created_at;
+
+    protected ?Carbon $updated_at;
+
+    #[Relation]
+    public User $user;
+
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,12 +4,13 @@ namespace App\Models;
 
 use Based\Fluent\Fluent;
 use Based\Fluent\Relations\Relation;
-use Carbon\Carbon;
 use Exception;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Notifications\Notification;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Log;
 use Laravel\Sanctum\HasApiTokens;
@@ -46,7 +47,7 @@ class User extends Authenticatable
 
     protected Carbon $created_at;
 
-    protected Carbon $updated_at;
+    protected ?Carbon $updated_at;
 
     protected string $locale;
 
@@ -55,12 +56,12 @@ class User extends Authenticatable
 
     public Collection $socialLogins;
 
-    public function notificationTokens()
+    public function notificationTokens(): HasMany
     {
         return $this->hasMany(NotificationToken::class);
     }
 
-    public function socialLogins()
+    public function socialLogins(): HasMany
     {
         return $this->hasMany(SocialLogin::class);
     }
