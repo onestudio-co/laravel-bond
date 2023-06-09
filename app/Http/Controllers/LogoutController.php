f49730aa2021c4de95b92998576c06d9fb175b34
Hala S.A. Abusalim <hala.s.salim@gmail.com>
2023-06-09 22:00:43 +0300

    create destroy function and refactor the logout

--- app/Http/Controllers/LogoutController.php
+++ app/Http/Controllers/LogoutController.php
@@ -2,12 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\DeletedUser;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 class LogoutController extends Controller
 {
-    public function logout()
+    public function logout(Request $request)
     {
-        auth()->user()->tokens()->delete();
+        $request->user()->tokens()->delete();
 
         return response()->json(['message' => 'logout success']);
+    }
+
+    public function destroy(Request $request)
+    {
+        /** @var User $user */
+        $user = Auth::user();
+
+        event(new DeletedUser($user));
+
+        $user->delete();
+        return response()->json(['message' => 'account deleted successfully']);
     }
 }
