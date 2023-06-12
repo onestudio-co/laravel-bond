49583a6ff690dbb8dc8583daeb2c4d9a3d85ddfa
Mohamed AlWhaidi <49125190+MohamedAlwhaidi@users.noreply.github.com>
2023-06-12 23:52:41 +0300

    fix style

--- app/Models/NotificationToken.php
+++ app/Models/NotificationToken.php
@@ -8,5 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class NotificationToken extends Model
 {
     use HasFactory;
+
     protected $guarded = [];
 }
--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -11,7 +11,7 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
 {
     public function register(): void
     {
-//         Telescope::night();
+        //         Telescope::night();
 
         $this->hideSensitiveRequestDetails();
 
