a586c11e4cc627eabf1a52db7f2c6d5102092991
Mohamed AlWhaidi <49125190+MohamedAlwhaidi@users.noreply.github.com>
2023-06-12 23:00:11 +0300

    upgrade socialite provider

--- app/Rules/ActiveUserEmail.php
+++ app/Rules/ActiveUserEmail.php
@@ -10,7 +10,7 @@ class ActiveUserEmail implements ValidationRule
 {
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-        if (User::query()->where('email', $value)->whereNull('deleted_at')->count() === 0) {
+        if (User::query()->where('email', $value)->whereNull('deleted_at')->count() !== 0) {
             $fail('validation.active_user')->translate();
         }
     }
