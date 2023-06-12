d9dc768dd4be4cfad809258875eea7960b0abdb1
Mohamed AlWhaidi <49125190+MohamedAlwhaidi@users.noreply.github.com>
2023-06-12 15:52:54 +0300

    upgrade Models

--- app/Models/NotificationToken.php
+++ app/Models/NotificationToken.php
@@ -2,27 +2,11 @@
 
 namespace App\Models;
 
-use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class NotificationToken extends Model
 {
     use HasFactory;
-
     protected $guarded = [];
-
-    public int $id;
-
-    public int $user_id;
-
-    public string $token;
-
-    public string $device_id;
-
-    public string $device_type;
-
-    public Carbon $created_at;
-
-    public ?Carbon $updated_at = null;
 }
