4c0edcba21665d86ea3c85cdda2741485157a471
Obeida Raida <70585602+obeidareda37@users.noreply.github.com>
2023-06-16 00:20:53 +0300

    create migration to drop unique [email,deleted_at] constraint from users table and make email unique

---
+++ database/migrations/2023_06_15_202456_drop_unique_constraint_email_and_deleted_at_from_users_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unique('email');
+
+        });
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropUnique(['email', 'deleted_at']);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unique(['email', 'deleted_at']);
+        });
+    }
+};
