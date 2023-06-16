8a4154a0d1bf5b9c5d33ed15c7637eb891f7da4d
Mohamed AlWhaidi <49125190+MohamedAlwhaidi@users.noreply.github.com>
2023-06-16 17:04:27 +0300

    fix style

--- database/migrations/2023_06_16_140144_add_expires_at_to_personal_access_tokens.php
+++ database/migrations/2023_06_16_140144_add_expires_at_to_personal_access_tokens.php
@@ -4,14 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::table('personal_access_tokens', function (Blueprint $table) {
-            if (!Schema::hasColumn('personal_access_tokens', 'expires_at')) {
+            if (! Schema::hasColumn('personal_access_tokens', 'expires_at')) {
                 $table->timestamp('expires_at')->nullable();
             }
         });
