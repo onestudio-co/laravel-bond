f9a17ee076f653a48c8203ff95f9a1cf8c8292f8
Hala S.A. Abusalim <hala.s.salim@gmail.com>
2023-06-22 11:46:05 +0300

    user controller show &  update

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UpdateUserRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+
+    public function show(Request $request)
+    {
+        /** @var User $user */
+        $user = $request->user();
+
+        return UserResource::make($user);
+
+    }
+
+    public function update(UpdateUserRequest $request)
+    {
+        /** @var User $user */
+        $user = $request->user();
+        $user->update($request->validated());
+
+        return UserResource::make($user);
+    }
+}
