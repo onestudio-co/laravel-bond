78ff1132cce4b8b9acd8205b25a51ff963ea0926
Hala S.A. Abusalim <hala.s.salim@gmail.com>
2023-07-06 00:43:11 +0300

    install and config twill cms

---
+++ config/twill.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    /*
+  |--------------------------------------------------------------------------
+  | Twill app locale // the default locale for the Twill admin is set to 'en'
+  | eg: we can change it to 'ar' to make the admin RTL
+  |--------------------------------------------------------------------------
+  |
+  */
+    'locale' => 'en'
+];
